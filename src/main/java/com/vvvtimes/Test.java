540b6a84f2388786725d5eefc4ec98ab036da3e8
Administrator <Administrator>
2017-12-12 10:13:23 +0800

    初始化

---
+++ src/main/java/com/vvvtimes/Test.java
@@ -0,0 +1,12 @@
+package com.vvvtimes;
+
+import java.util.Date;
+
+/**
+ * Created by shicz on 2017/12/12.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(new Date(1691839999000L));
+    }
+}
